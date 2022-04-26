54be9c4485f9de009222815b8a36a65f33c297d9
vargasce <cris.ema.vargas20@gmail.com>
2022-04-26 20:54:29 -0300

    environment prod conf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   apiURL : 'https://seecourses.herokuapp.com/api/',
-  //apiURL : 'http://192.168.1.14:3700/api/',
   headers : { 'Content-Type' : 'application/json' },
   token : sessionStorage.getItem('token') ,
   id_entidad : sessionStorage.getItem('id_entidad') ,
